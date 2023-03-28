ac83fbf5bb133bd60187516bf17b743e5539c1a0
Crowdbotics <team@crowdbotics.com>
2023-03-28 14:47:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-leaf-39584",
+  url: "https://misty-leaf-39584.botics.co",
+  api: "https://misty-leaf-39584.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
